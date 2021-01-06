5b5ad2ef7c4d06ee71d5663df5f7de1874cc0e13
Jonas Mittnacht <rizecookey@gmail.com>
2021-01-06 15:51:07 +0100

    Small changes

--- factory-builder/src/main/java/com/combatreforged/factory/factorybuilder/FactoryBuilder.java
+++ factory-builder/src/main/java/com/combatreforged/factory/factorybuilder/FactoryBuilder.java
@@ -1,14 +1,13 @@
 package com.combatreforged.factory.factorybuilder;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class FactoryBuilder implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("Factory Builder");
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		System.out.println("Hello Fabric world!");
+		LOGGER.info("Initializing Factory Builder...");
 	}
 }
