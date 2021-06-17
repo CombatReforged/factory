14c2944c04b93321b3585edc5dc4677a8d7632fd
rizecookey <cookie.mijo@gmail.com>
2021-06-17 12:03:17 +0200

    ScoreboardTeam (partially, unfinished)

--- factory-api/src/main/java/com/combatreforged/factory/api/world/scoreboard/ScoreboardTeam.java
+++ factory-api/src/main/java/com/combatreforged/factory/api/world/scoreboard/ScoreboardTeam.java
@@ -1,6 +1,23 @@
 package com.combatreforged.factory.api.world.scoreboard;
 
+import net.kyori.adventure.text.Component;
+
+import java.util.Set;
+
 public interface ScoreboardTeam {
+    Scoreboard getScoreboard();
     String getName();
-    //TODO
+    Set<String> getPlayers();
+    Component getDisplayName();
+    void setDisplayName(Component displayName);
+    Component getPrefix();
+    void setPrefix(Component prefix);
+    Component getSuffix();
+    void setSuffix(Component suffix);
+    boolean isFriendlyFire();
+    void setFriendlyFire(boolean friendlyFire);
+    boolean canSeeInvisibleMates();
+    void setSeeInvisibleMates(boolean seeInvisibleMates);
+
+    //TODO rest
 }
