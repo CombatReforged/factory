99786bc0249950cffe39f7b3f0e511665e91289d
Jonas Mittnacht <rizecookey@gmail.com>
2021-06-20 19:32:54 +0200

    Small stuff

--- factory-api/src/main/java/com/combatreforged/factory/api/world/item/container/menu/ContainerMenu.java
+++ factory-api/src/main/java/com/combatreforged/factory/api/world/item/container/menu/ContainerMenu.java
@@ -1,5 +1,13 @@
 package com.combatreforged.factory.api.world.item.container.menu;
 
+import com.combatreforged.factory.api.world.item.ItemStack;
+
+import java.util.List;
+import java.util.Map;
+
 public interface ContainerMenu {
-    //TODO
+    Map<Integer, ItemStack> getItems();
+    ItemStack getItem(int slot);
+    void setItem(int slot, ItemStack itemStack);
+    boolean isEmpty(int slot); //TODO continue
 }
