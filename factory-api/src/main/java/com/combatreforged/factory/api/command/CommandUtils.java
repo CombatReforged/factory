9fa40ca4e7af378c0dd6910fdccb2a730a3e12b6
Jonas Mittnacht <rizecookey@gmail.com>
2021-10-02 16:50:03 +0200

    Expand command utils xd

--- factory-api/src/main/java/com/combatreforged/factory/api/command/CommandUtils.java
+++ factory-api/src/main/java/com/combatreforged/factory/api/command/CommandUtils.java
@@ -1,11 +1,17 @@
 package com.combatreforged.factory.api.command;
 
+import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 
 public final class CommandUtils {
     private CommandUtils() {}
 
     public static LiteralArgumentBuilder<CommandSourceInfo> literal(String literal) {
         return LiteralArgumentBuilder.literal(literal);
+    }
+
+    public static <T> RequiredArgumentBuilder<CommandSourceInfo, T> argument(String name, ArgumentType<T> argumentType) {
+        return RequiredArgumentBuilder.argument(name, argumentType);
     }
 }
