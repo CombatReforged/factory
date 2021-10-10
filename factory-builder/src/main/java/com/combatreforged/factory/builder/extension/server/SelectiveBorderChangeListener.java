38adb1c81c30000ff7352d1d9317144a819e10b9
Jonas Mittnacht <rizecookey@gmail.com>
2021-10-10 16:56:45 +0200

    Fixes

--- factory-builder/src/main/java/com/combatreforged/factory/builder/extension/server/SelectiveBorderChangeListener.java
+++ factory-builder/src/main/java/com/combatreforged/factory/builder/extension/server/SelectiveBorderChangeListener.java
@@ -15,29 +15,36 @@ public class SelectiveBorderChangeListener implements BorderChangeListener {
         this.dimensions = dimensions;
     }
 
+    @Override
     public void onBorderSizeSet(WorldBorder worldBorder, double d) {
         this.sendPacket(new ClientboundSetBorderPacket(worldBorder, ClientboundSetBorderPacket.Type.SET_SIZE));
     }
 
+    @Override
     public void onBorderSizeLerping(WorldBorder worldBorder, double d, double e, long l) {
         this.sendPacket(new ClientboundSetBorderPacket(worldBorder, ClientboundSetBorderPacket.Type.LERP_SIZE));
     }
 
+    @Override
     public void onBorderCenterSet(WorldBorder worldBorder, double d, double e) {
         this.sendPacket(new ClientboundSetBorderPacket(worldBorder, ClientboundSetBorderPacket.Type.SET_CENTER));
     }
 
+    @Override
     public void onBorderSetWarningTime(WorldBorder worldBorder, int i) {
         this.sendPacket(new ClientboundSetBorderPacket(worldBorder, ClientboundSetBorderPacket.Type.SET_WARNING_TIME));
     }
 
+    @Override
     public void onBorderSetWarningBlocks(WorldBorder worldBorder, int i) {
         this.sendPacket(new ClientboundSetBorderPacket(worldBorder, ClientboundSetBorderPacket.Type.SET_WARNING_BLOCKS));
     }
 
+    @Override
     public void onBorderSetDamagePerBlock(WorldBorder worldBorder, double d) {
     }
 
+    @Override
     public void onBorderSetDamageSafeZOne(WorldBorder worldBorder, double d) {
     }
 
--- factory-builder/src/main/java/com/combatreforged/factory/builder/mixin/__bugfixes/UpdateAfterChangeWorldFix.java
+++ factory-builder/src/main/java/com/combatreforged/factory/builder/mixin/__bugfixes/UpdateAfterChangeWorldFix.java
@@ -2,22 +2,15 @@ package com.combatreforged.factory.builder.mixin.__bugfixes;
 
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayer.class)
-public abstract class UpdateAfterChangeWorldFix extends LivingEntity {
-    protected UpdateAfterChangeWorldFix(EntityType<? extends LivingEntity> entityType, Level level) {
-        super(entityType, level);
-    }
-
+public abstract class UpdateAfterChangeWorldFix {
     @Inject(method = "teleportTo(Lnet/minecraft/server/level/ServerLevel;DDDFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;setLevel(Lnet/minecraft/world/level/Level;)V", shift = At.Shift.AFTER))
-    private void sendNBT(ServerLevel serverLevel, double d, double e, double f, float g, float h, CallbackInfo ci) {
+    public void sendNBT(ServerLevel serverLevel, double d, double e, double f, float g, float h, CallbackInfo ci) {
         FixUtils.updateDelayed((ServerPlayer) (Object) this);
     }
 }
